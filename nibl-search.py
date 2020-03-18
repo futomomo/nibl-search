4376b00479f8796593002552bf313a0d1dd323ae
futomomo <trashman3k@gmail.com>
2020-03-18 02:59:12 +0100

    turns out nibl had a json api lmao

--- nibl-search.py
+++ nibl-search.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env py -2
 import urllib
-import xml.dom.minidom
+import json
 import hexchat
 
 lastSearch = []
+botList = {}
 
 __module_name__ = 'NIBL XDCC Search'
 __module_version__ = '1.0'
@@ -18,17 +19,44 @@ helpUsage = '''Usage: NIBL search <search query>
 class AppURLopener(urllib.FancyURLopener):
     version = 'nibl-search/1.0'
 
-
 urllib._urlopener = AppURLopener()
+
+def udateBotList():
+    global botList
+    botUrl = 'https://api.nibl.co.uk/nibl/bots'
+    stringResult = ''
+    try:
+        fileObj = urllib.urlopen(finalUrl)
+        # print('The HTTP status code was: ' + str(fileObj.getcode()))
+        if fileObj.getcode() != 200:
+            fileObj.close()
+            return
+        for lines in fileObj.readlines():
+            if len(lines) == 0:
+                print('Empty response')
+                fileObj.close()
+                return
+            else:
+                for line in lines:
+                    stringResult += line
+        fileObj.close()
+    except IOError:
+        print('IOError: unable to make a connection to url')
+        return
+    jsonResult = json.loads(stringResult)['content']
+    for bot in jsonResult:
+        botList[str(bot['id'])] = bot['name']
+    return
 
 def search(word, word_eol, userdata):
     global lastSearch
-    baseUrl = 'https://nibl.co.uk/bots.php?search='
+    global botList
+    baseUrl = 'https://api.nibl.co.uk/nibl/search?query='
     searchQuery = word_eol[2]
     safeQuery = urllib.quote_plus(searchQuery)
     finalUrl = baseUrl+safeQuery
     # print('Final url: ' + finalUrl)
-    documentString = ''
+    stringResult = ''
     # print('Opening \'' +  finalUrl + '\'')
     urllib.urlcleanup()
     try:
@@ -36,48 +64,37 @@ def search(word, word_eol, userdata):
         # print('The HTTP status code was: ' + str(fileObj.getcode()))
         if fileObj.getcode() != 200:
             fileObj.close()
-            return []
+            return
         for lines in fileObj.readlines():
             if len(lines) == 0:
                 print('Empty response')
+                fileObj.close()
+                return
             else:
                 for line in lines:
-                    documentString += line
+                    stringResult += line
         fileObj.close()
     except IOError:
         print('IOError: unable to make a connection to url')
         return
-    document = xml.dom.minidom.parseString(documentString)
-    items = []
-
-    for tableRow in document.getElementsByTagName('tr'):
-        if tableRow.getAttribute('class').find('botlistitem') != -1:
-            botName = tableRow.getAttribute('botname')
-            botPack = tableRow.getAttribute('botpack')
-            filename = ''
-            for td in tableRow.getElementsByTagName('td'):
-                if td.getAttribute('class') == 'filename':
-                    if td.firstChild.nodeType == td.TEXT_NODE:
-                        filename = td.firstChild.data.rstrip()
-            items.append({'botName': botName, 'botPack': botPack, 'filename': filename})
-    lastSearch = items[:]
-    document = None
+    jsonResult = json.loads(stringResult)['content']
+    lastSearch = jsonResult[:]
     tabContext = hexchat.find_context(channel=tabName)
     if tabContext is None:
         hexchat.command('QUERY {}'.format(tabName))
         tabContext = hexchat.find_context(channel=tabName)
 
-    tabContext.emit_print('Generic Message', 'SEARCH', 'Query \'' + searchQuery + '\'\nFound ' + str(len(items)) + ' results')
-    resultString = ''
-    for i,item in enumerate(items):
-        itemString = '{}. \035\00307{}\017 \002----\017 \00311/msg {} xdcc send {}\017\n'.format(i+1, item['filename'], item['botName'], item['botPack'])
-        if len(resultString) >= 2000:
-            tabContext.emit_print('Generic Message', '', resultString.rstrip())
-            resultString = itemString
+    tabContext.emit_print('Generic Message', 'SEARCH', 'Query \'' + searchQuery + '\'\nFound ' + str(len(jsonResult)) + ' results')
+    outString = ''
+    for i,item in enumerate(jsonResult):
+        itemString = '{}. \035\00307{}\017 \002----\017 \00311/msg {} xdcc send {}\017\n'.format(i+1, item['name'], botList[str(item['botId'])], item['number'])
+        if len(outString) + len(itemString) > 2000:
+            tabContext.emit_print('Generic Message', '', outString.rstrip())
+            outString = itemString
         else:
-            resultString += itemString
+            outString += itemString
     if len(resultString) > 0:
-        tabContext.emit_print('Generic Message', '', resultString.rstrip())
+        tabContext.emit_print('Generic Message', '', outString.rstrip())
     return
 
 def download(word, word_eol, userdata):
@@ -99,8 +116,8 @@ def download(word, word_eol, userdata):
         tabContext.emit_print('Generic Message', 'GET', '\002\00304ERROR: Download index is out of range.\017')
         return
     itemToGet = lastSearch[indexToGet]
-    commandString = 'MSG {} xdcc send {}'.format(itemToGet['botName'], itemToGet['botPack'])
-    tabContext.emit_print('Generic Message', 'GET', 'GETting \035\00307{}\017'.format(itemToGet['filename']))
+    commandString = 'MSG {} xdcc send {}'.format(botList[str(itemToGet['botId'])], itemToGet['number'])
+    tabContext.emit_print('Generic Message', 'GET', 'GETting \035\00307{}\017'.format(itemToGet['name']))
     tabContext.command(commandString)
     return
 
